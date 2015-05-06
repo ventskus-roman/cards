83e17c85952c970a99b675217c44672f9d45fb7b
Roman Ventskus <scofield665@gmail.com>
2015-05-06 19:42:57 +0300

    Validation and simple_form gem

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,13 @@
 class Card < ActiveRecord::Base
+  validates :original_text, presence: true
+  validates :translated_text, presence: true
+  validates :review_date, presence: true
+  validate :check_original_and_translated_not_equals
+
+  def check_original_and_translated_not_equals
+    if original_text.downcase == translated_text.downcase
+      errors.add(:translated_text, "can`t be the same as original text")
+    end
+  end
+
 end
