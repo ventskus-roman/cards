c4351708ca46b74eeb86d435c91d344a8695f6e4
Roman Ventskus <scofield665@gmail.com>
2015-05-09 14:33:54 +0300

    seed.rb for cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'open-uri'
+
+@doc = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/common-german-words"))
+@doc.css("tr[class^=row]").each do |row|
+  columns = row.css("td")
+  Card.create(original_text: columns[1].content,
+              translated_text: columns[2].content)
+end
